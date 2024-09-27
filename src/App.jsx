f88fe2a52cdc1bc509ba9b26412c96bb0e1a1a94
travisneto <travisneto@live.com>
2024-09-27 09:15:41 -0400

    Working on components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import MailboxDetails from "./components/MailboxDetails"
+import MailboxForm from "./MailboxForm"
+import MailboxList from "./components/MailboxList"
+import NavBar from "./components/NavBar"
+import { Route, Routes } from "react-router-dom"
+import { useState } from "react"
+import "./App.css"
+
+
 // src/App.jsx
 
 const App = () => {
-  return <h1>Hello world!</h1>;
-};
+  const [mailboxes, setMailboxes] = useState([])
 
-export default App;
+  const addBox = () => {
+    setMailboxes()
+  }
 
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <h1>Post Office</h1>
+            </main>
+          }
+        />
+        <Route path="/mailboxes" element={<MailboxList />} />
+        <Route path="/new-mailbox" element={<MailboxForm />} />
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
